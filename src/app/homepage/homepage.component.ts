effd38abf89a19869abdaa4efdaef78f1a60f0e7
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-07-29 18:05:50 +0000

    Create homepage component and include in app-root file

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,14 @@
+// File handles the main body of the app homepage.
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}
