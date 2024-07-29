e083f717e7848582229a9d868f4097783b39d896
TedmanNguyen <tedmanln@gmail.com>
2024-07-29 11:41:22 -0700

    Basic API call setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HttpClientModule],
+  template: `<h1>Hello world!</h1>`,
+  
 })
 export class AppComponent {
   title = 'soseproject';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getData('pokemon/ditto').subscribe(data => {
+      console.log(data);
+    });
+  }
+
 }
