53df3c22973a81e87301d8911625aed50796f335
TedmanNguyen <tedmanln@gmail.com>
2024-07-29 12:17:10 -0700

    test api call on component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
-// File handles and contains mainpage elements.
-
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './api.service';
-=======
-import { HomepageComponent } from './homepage/homepage.component';
->>>>>>> bf089678ad157eab56cc1dfc08f882bf8ad71362
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet],
   template: `<h1>Hello world!</h1>`,
   
-=======
-  imports: [RouterOutlet, HomepageComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
->>>>>>> bf089678ad157eab56cc1dfc08f882bf8ad71362
 })
 export class AppComponent {
   title = 'soseproject';
