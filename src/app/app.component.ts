6985d6c633dd5d593958762dad7fc0c8755da0e0
TedmanNguyen <tedmanln@gmail.com>
2024-07-29 12:33:57 -0700

    updated commit to include from main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './api.service';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Hello world!</h1>`,
+  imports: [RouterOutlet, HomepageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
   
 })
 export class AppComponent {
