0ae34147b1bca986ee5296a0c319f7d9184276d1
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-07-31 20:36:54 +0000

    Create draft of info page

--- src/app/infopage/infopage.component.ts
+++ src/app/infopage/infopage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-infopage',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule, MatCardModule],
   templateUrl: './infopage.component.html',
   styleUrl: './infopage.component.css'
 })
