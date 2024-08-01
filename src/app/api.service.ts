ce45bfb2a3b8d9fec796e14e183aa9701219fef7
TedmanNguyen <tedmanln@gmail.com>
2024-08-01 11:50:29 -0700

    game generation doc translates game to gen. API testing methods

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,12 +8,26 @@ import { Observable } from 'rxjs';
 export class ApiService {
 
   private baseUrl = 'https://pokeapi.co/api/v2/';
+  gamesList: Observable<any>[] = [];
+
   
   constructor(private http: HttpClient) { }
 
   getData(endpoint: string): Observable<any> {
     return this.http.get(`${this.baseUrl}/${endpoint}`);
   }
+  
+  /*
+  getVersionGroup(game: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/version/${game}`);
+  }
+
+  getGame(gameNumber : number): Observable<any> {
+    //get a single instance of a game based on the parameter
+    return this.http.get(`${this.baseUrl}/version/?limit=30`);
+  }
+  */
+
   postData(endpoint: string, data: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/${endpoint}`, data);
   }
