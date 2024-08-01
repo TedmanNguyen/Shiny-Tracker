677de6b91c4e84416e61c158b384fe88c34d4cad
Sergio Zavala <sezavala@csumb.edu>
2024-08-01 15:20:57 -0700

    Added dropdown gen options

--- src/app/hunt-instance/hunt-instance.component.ts
+++ src/app/hunt-instance/hunt-instance.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameGenerationService } from '../game-generation.service';
 
 @Component({
   selector: 'app-hunt-instance',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hunt-instance.component.html',
   styleUrls: ['./hunt-instance.component.css'],
 })
-export class HuntInstanceComponent {}
+export class HuntInstanceComponent {
+  gameGenerations: string[] = [];
+
+  constructor(private gameGenerationService: GameGenerationService) {}
+
+  ngOnInit(): void {
+    this.gameGenerationService.getGameGenerations().subscribe((data) => {
+      this.gameGenerations = Object.keys(data);
+    });
+  }
+}
