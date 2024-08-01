389107b81081a5cdac72d75e73ab8dffcfdcb43d
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-08-01 19:31:41 +0000

    Include pictures of normal vs. shiny pokemon

--- src/app/infopage/infopage.component.ts
+++ src/app/infopage/infopage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-infopage',
   standalone: true,
-  imports: [MatTabsModule, MatCardModule],
+  imports: [MatTabsModule, MatCardModule, MatGridListModule],
   templateUrl: './infopage.component.html',
   styleUrl: './infopage.component.css'
 })
