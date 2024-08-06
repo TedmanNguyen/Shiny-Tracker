a77181d1e0db585631b0907b1fd119a26a19afe4
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-08-06 18:32:39 +0000

    Included helper functions for hunting methods that require the count of the chain

--- src/app/rate-generation.service.ts
+++ src/app/rate-generation.service.ts
@@ -9,6 +9,13 @@ import { map } from 'rxjs/operators';
 export class RateGenerationService {
   private jsonUrl = 'assets/shiny-methods.json';
 
+  // for use to calculate gen iv radar rates
+  private radarNumerators = [8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 
+                             12, 12, 13, 13, 14, 14, 15, 15, 16, 17, 18,
+                             19, 20, 21, 22, 24, 26, 28, 30, 33, 37, 41, 
+                             47, 55, 66, 82, 110, 164, 328]
+
+
   constructor(private http: HttpClient) {}
 
   // obtains rates and outputs as a TS object
@@ -18,7 +25,7 @@ export class RateGenerationService {
     );
   }
 
-// returns the string corresponding to the selected rate from JSON
+  // returns the string corresponding to the selected rate from JSON
   getSelectedRate(userMethod: string, userGen: string) {
     // load rates fron JSON
     this.getRates().subscribe(rates => {
@@ -31,5 +38,43 @@ export class RateGenerationService {
       // returns the rate as a string, or undefined if invalid parameters
       return rate;
     });
+  }
+
+  // calculates the rate for chain fishing
+  getChainFishingRate(chain: number, hasCharm: boolean) {
+    let charmRate = hasCharm ? 2 : 0;
+    if (chain < 20) {
+      return `${charmRate + 1 + 2 * chain}/4096`;
+    }
+    else {
+      return `${charmRate + 41}/4096`;  // since base rate caps at 41/4096
+    }
+  }
+
+  // calculates the rate for SOS chaining
+  getSOSRate(chain: number, hasCharm: boolean) {
+    let charmRate = hasCharm ? 2 : 0;
+    if (chain <= 10) {
+      return `${charmRate + 1}/4096`;
+    }
+    else if (chain <= 20) {
+      return `${charmRate + 5}/4096`;
+    }
+    else if (chain < 30) {
+      return `${charmRate + 9}/4096`;
+    }
+    else {
+      return `${charmRate + 13}/4096`;  // since base rate caps at 13/4096
+    }
+  }
+
+  //calculates the rate for generation iv Poke Radar
+  getRadarRate(chain: number) {
+    if (chain < 40) {
+      return `${this.radarNumerators[chain]}/65536`;
+    }
+    else {
+      return `${this.radarNumerators[40]}/65536`;
+    }
   }
 }
