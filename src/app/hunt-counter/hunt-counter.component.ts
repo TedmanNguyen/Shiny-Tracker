a5f225b823a4a2bb02be3ba6b69619ba9b9947e8
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-08-07 18:27:40 +0000

    Loaded each hunt in cookie array into main page

--- src/app/hunt-counter/hunt-counter.component.ts
+++ src/app/hunt-counter/hunt-counter.component.ts
@@ -1,37 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CookieService } from 'ngx-cookie-service'; 
 import { HuntInstanceComponent } from '../hunt-instance/hunt-instance.component';
+import { RateGenerationService } from '../rate-generation.service';
+
+interface HuntCard {
+  game: string;
+  generation: string;
+  pokemon: string;
+  method: string;
+  found: boolean;
+  counter: Counter;
+}
+
+interface Counter {
+  encounters: number;
+  rate: string;
+}
 
 @Component({
   selector: 'app-hunt-counter',
   standalone: true,
-  imports: [HuntInstanceComponent],
+  imports: [CommonModule, HuntInstanceComponent],
   templateUrl: './hunt-counter.component.html',
   styleUrl: './hunt-counter.component.css'
 })
 export class HuntCounterComponent {
-  constructor(private cookieService: CookieService) {}
+  huntInstances: any[] = [];
 
-  loadHuntInstances(): void {
-    //huntInstances: HuntInstance[] = [];
-
-
-    const storedHuntInstances = this.cookieService.get('huntInstances');
-    if (storedHuntInstances) {
-      //this.huntInstances = JSON.parse(storedHuntInstances);
-    }
-  }
-
+  constructor(private cookieService: CookieService,
+              private rateGenerationService: RateGenerationService
+  ) {}
 
   ngOnInit(): void {
-    const storedHuntInstances = this.cookieService.get('huntInstances');
-    if (storedHuntInstances) {
-      let huntInstances = JSON.parse(storedHuntInstances);
-      for (let huntInstance of huntInstances) {
-        // function to intialize an individual hunt instance
-      }
-    }
+    this.huntInstances = this.loadHuntInstances();
+      console.log(this.huntInstances);
   }
 
-
+  loadHuntInstances(): any {
+    const huntInstanceCookie = this.cookieService.get('huntInstances');
+    if (huntInstanceCookie) {
+      return JSON.parse(huntInstanceCookie);
+    }
+    return [];
+  }
 }
